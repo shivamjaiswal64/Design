875e80334ed2f12979a0c6dc4b2a03a7fdabf4cd
SHIVAM JAISWAL <39907285+shivamjaiswal64@users.noreply.github.com>
2019-08-15 03:04:55 +0530

    Updated design2.java
    Minor changes only in syntax for better Readabilty. Source code is remains same.

--- design2.java
+++ design2.java
@@ -1,41 +1,41 @@
 import java.util.*;
 class design2{
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		int n=new Scanner(System.in).nextInt();
 		System.out.println("Number="+n);
-		for (int k=n;k>0 ;k-- ) {
+		for (int k=n;k>0 ;k-- ){
 			for(int i=1;i<k;i++){	//upper half, quadrant IV
 				System.out.print("--");
 			}
 			for (int j=n;j>k-1 ;j-- ) {
 				System.out.print("-"+j);
 			}
-			//for(int i=1;i<n;i++){
+			
 			for(int j=k+1;j<=n;j++){	//upper half, quadrant I
 				System.out.print("-"+j);
 			}
 			for(int i=1;i<k;i++)
-			System.out.print("--");		//mid half end
+				System.out.print("--");		//mid half end
 			
-		System.out.println()	;
-
-	}
-	for (int k=1;k<n ;k++ ) {
-		//System.out.print(" ");
-		for(int i=1;i<=k;i++){		//Lower half , Quadrant III
-				System.out.print("--");
-			}
-		for (int j=n;j>k ;j-- ) {
-				System.out.print("-"+j);
+			System.out.println()	;
 		}
-			for(int j=k+2;j<=n;j++){	//Lower half, Quadrant II
-				System.out.print("-"+j);
-			}
-			for(int i=1;i<=k;i++)
-				System.out.print("--");//
-			System.out.println();
 		
-	}
+	for (int k=1;k<n ;k++ ) {
+		
+		for(int i=1;i<=k;i++){		//Lower half , Quadrant III
+			System.out.print("--");
+		}
+		for (int j=n;j>k ;j-- ) {
+			System.out.print("-"+j);
+		}
+		for(int j=k+2;j<=n;j++){	//Lower half, Quadrant II
+			System.out.print("-"+j);
+		}
+		for(int i=1;i<=k;i++)
+			System.out.print("--");
+		
+		System.out.println();
+	}					//Lower Half Ends
 
-	}
+    }
 }
